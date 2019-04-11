ab01efdea35bda0d55b7841983175636762f5b46
TakeshimaNahoko <takeshima.nahoko@lancers.co.jp>
2019-04-11 21:36:36 +0900

    fix: fix CustomerClass

--- 01-refactoring/customer.java
+++ 01-refactoring/customer.java
@@ -14,12 +14,50 @@ class Customer {
     retunr _name;
   }
 
-  public String etatement() {
+  public String statement() {
     double totalAmount = 0;
     int frequentRenterPoints  = 0;
     Enumeration rentals = _rentals.addElements();
     String result = "Rental Record for " + getName() + "\n";
+    while (rentals.hasMoreElements()) {
+      double thisAmount = 0;
+      Rental each = (Reantal) rentals.nextElement();
 
-    // TODO
+      //一行ごとに金額を計算
+      switch (each.getMovie().getPriceCode()) {
+        case Movie.REGULAR:
+          this.Amount += 2;
+          if (each.getDaysRented() > 2) {
+            thisAmount += (each.getDaysRented() - 2) * 1.5;
+          }
+          break;
+        case Movie.NEW_RELEASE:
+          thisAmount += each.getDaysRented() * 3;
+          break;      
+        case Movie.CHILDRENS:
+          thisAmount += 1.5;
+          if (each.getDaysRented() > 3) {
+            thisAmount += (each.getDaysRented() - 3) * 1.5;
+          }
+          break;
+      }
+
+      // レンタルポイント加算
+      frequentRenterPoints++;
+      
+      //新作を２日以上借りた場合はボーナスポイント
+      if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1) {
+        frequentRenterPoints++;
+      }
+
+      // この貸出に関する数値の表示
+      result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(thisAmount) + "\n";
+      totalAmount += thisAmount;
+    }
+
+    // フッタ部分の追加
+    result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
+    result + = "you earned "String.valueOf(frequentRenterPoints) + " frequent renter ponts";
+    return result;
   }
 }
